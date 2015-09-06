76338590ac4c34ec3d1c7f317a47670236403e8b
Szymon Ziolkowski <nomyzs.z@gmail.com>
2015-09-06 12:32:52 +0200

    added empty classes for fingerprint scanning

---
+++ src/main/java/client/FutronicFingerprintScanner.java
@@ -0,0 +1,25 @@
+package client;
+
+import java.util.ArrayList;
+
+/**
+ * Created by simon on 2015-09-06.
+ */
+public class FutronicFingerprintScanner implements FingerPrintScannerInterface {
+
+    public ArrayList<byte[]> getImages()
+    {
+        //this can only be tested with futronic device when run on pi. wait can look into windows java as well will do.
+
+
+
+        return null;
+    }
+
+    public ArrayList<byte[]> captureFrames()
+    {
+
+
+        return null;
+    }
+}
